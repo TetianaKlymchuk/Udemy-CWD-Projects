d8f8abd1bdf5e17bb2e27a260cdee539365952ea
Tetiana Klymchuk <missklimchuck@yandex.ua>
2018-12-05 23:55:20 +0100

    Added 7th App Skeleton and decoration

---
+++ 8. Drawing App (jQueryUI & HTML5 Canvas)/javascript.js
@@ -0,0 +1,13 @@
+//javascript.js
+$(function(){
+
+    //change lineWidth using slider
+    $("#slider").slider({
+        min: 3,
+        max: 30,
+        slide: function(event, ui){
+            $("#circle").height(ui.value);
+            $("#circle").width(ui.value);
+        }
+    });
+});
